dd72363d5d3821bf2a5bf78c6ba606734ce8c978
nickjohngray <nickjohngray@gmail.com>
2020-08-03 22:54:29 +1200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import "./App.css";
+import {ContentToggler} from "@nickjohngray/blockout";
+
+const App = () =>
+    <div  className="App">
+      <ContentToggler   title="Toggle" >
+        <div>
+          Content goes here
+        </div>
+      </ContentToggler>
+
+    </div>
+
+export default App;
